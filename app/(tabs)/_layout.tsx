c3015098c467034e6f55fed0ad601d0b1c0bef1f
FIRR3 <150165932+FIRR3@users.noreply.github.com>
2025-11-21 09:36:05 +0100

    tabBar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,25 +1,82 @@
+import { icons } from "@/constants/icons";
 import { Tabs } from "expo-router";
 import React from "react";
+import { Image, ImageBackground, ImageSourcePropType, View } from "react-native";
+
+interface TabIconProps {
+  focused: boolean,
+  icons: ImageSourcePropType[],
+}
+
+const TabIcon = ({ focused, icons }: TabIconProps) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        className='flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full'
+      >
+        <Image source={icons.length > 1 ? icons[1] : icons[0]} className='size-7'/>
+      </ImageBackground>
+    )
+  }
+
+  return (
+    <View className='size-full justify-center items-center mt-4 rounded-full'>
+      <Image source={icons[0]} className='size-7'/>
+    </View>
+  )
+}
 
 const _Layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: '100%',
+          height: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'tomato'
+        },
+      }}
+    >
       <Tabs.Screen
         name="settings"
         options={{
-          title: "Settings"
+          title: "Settings",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icons={[icons.gearWhite, icons.gearBlue]}
+            />
+          )
         }}
       />
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home"
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icons={[icons.waterdrop]}
+            />
+          )
         }}
       />
       <Tabs.Screen
         name="statistics"
         options={{
-          title: "Statistics"
+          title: "Statistics",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icons={[icons.graphWhite, icons.gearBlue]}
+            />
+          )
         }}
       />
     </Tabs>
--- metro.config.js
+++ metro.config.js
@@ -2,5 +2,9 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
  
 const config = getDefaultConfig(__dirname)
+
+config.resolver.alias = {
+  "@": __dirname, // points @ to your project root
+};
  
 module.exports = withNativeWind(config, { input: './app/globals.css' })
