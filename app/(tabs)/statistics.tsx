4146c2b6fc818f940141b7efcce6dd13c4366120
Benjamin Saleem Brodin <benjaminbrodin07@gmail.com>
2026-01-11 10:37:20 +0100

    ble adaptation
    the app now connects to the device and recives and stores the data

--- app/(tabs)/statistics.tsx
+++ app/(tabs)/statistics.tsx
@@ -1,11 +1,14 @@
 import ScreenBackgroundWrapper from "@/components/ScreenBackgroundWrapper";
 import React from 'react';
 import { Text } from 'react-native';
+import WeightScreen from "@/components/weight_tester";
 
 const Statistics = () => {
   return (
     <ScreenBackgroundWrapper style={{ justifyContent: "center", alignItems: "center" }}>
       <Text className='text-5xl text-white'>Stats</Text>
+      <WeightScreen />
+      
     </ScreenBackgroundWrapper>
   )
 }
